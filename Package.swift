6693d55296df746a111346f41a3231bf75ee15a2
toshi0383 <t.suzuki326@gmail.com>
2016-01-21 00:33:43 +0900

    Add UnitTest

--- Package.swift
+++ Package.swift
@@ -1,5 +1,8 @@
 import PackageDescription
 
 let package = Package(
-    name: "Queue"
+    name: "Queue",
+    testDependencies: [
+        .Package(url: "https://github.com/kylef/spectre-build.git", majorVersion: 0),
+    ]
 )
